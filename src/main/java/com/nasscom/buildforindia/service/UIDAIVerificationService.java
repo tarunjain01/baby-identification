8269b980353f7387121e1c4f5b3f2347ec773133
Satyajit Chakraborty <satyajit2010@gmail.com>
2018-04-05 05:13:59 +0530

    renamed

--- src/main/java/com/nasscom/buildforindia/service/UIDAIVerificationService.java
+++ src/main/java/com/nasscom/buildforindia/service/UIDAIVerificationService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import com.nasscom.buildforindia.integration.uidai.OTPGenerator;
 
 @Service
-public class OTPVerificationService {
+public class UIDAIVerificationService {
 
 	@Autowired
 	@Qualifier("MockUIDAI")
